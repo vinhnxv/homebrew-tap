31cb9c46bafe0c1b75802a90f11962d094958c7f
Nguyễn Xuân Vĩnh <vinh.ngxv@gmail.com>
2026-03-15 03:41:10 +0700

    fix: update checksums for v0.3.0

--- Formula/melina.rb
+++ Formula/melina.rb
@@ -7,18 +7,18 @@ class Melina < Formula
   on_macos do
     on_arm do
       url "https://github.com/vinhnxv/melina/releases/download/v0.3.0/melina-v0.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "placeholder"
+      sha256 "fc53ed168ec19cf85160c1e6418ff407e515a7b27f1a4d078b23bb9307d9e0af"
     end
     on_intel do
       url "https://github.com/vinhnxv/melina/releases/download/v0.3.0/melina-v0.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "placeholder"
+      sha256 "62d5561edc2ca7a54aa4fc319d16f69e2e032424638d0a23f73e4a323bc1bd3e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/vinhnxv/melina/releases/download/v0.3.0/melina-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "placeholder"
+      sha256 "d4aea29dbffa54915607eb71ad5694c7551aa33935eb677ca4fd5b0cc084aa2d"
     end
   end
 
